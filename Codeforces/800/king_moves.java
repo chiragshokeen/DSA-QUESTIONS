ebbbac924aa9924205a42aa450528dd4a1d2da42
chiragshokeen <chirag.shokeen007@gmail.com>
2022-01-21 19:08:13 +0530

    commit

---
+++ Codeforces/800/king_moves.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.* ; 
+public class king_moves{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in) ;
+
+        String s = scn.next() ;
+
+        if( s.equals("a1") || s.equals("a8") || s.equals(h1) || s.equals("h8") ){
+            System.out.print("3") ; 
+            return ;
+        }
+
+
+
+
+
+    }
+    
+}
