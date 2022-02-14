eccaece989accc9df557f0d47b0126128f3aaf2f
chiragshokeen <chirag.shokeen007@gmail.com>
2022-02-14 20:54:59 +0530

    commit

---
+++ Love Babbar 400/10.(imp) min no of jumps.java
@@ -0,0 +1,37 @@
+class Solution{
+    static int minJumps(int[] arr){
+        // your code here
+        int n = arr.length;
+       int maxR = arr[0] ;
+       int jump = 1;
+       int step = arr[0] ;
+       
+       if(n == 1 ) return 0 ;
+      else if( arr[0] == 0 ) return -1;
+       
+       else{
+           for(int i = 1 ; i <n ; i++){
+               
+               if(i == n-1){
+                   return jump;
+               }
+           maxR = Math.max(maxR ,i + arr[i]  ) ;
+           
+           step--;
+           if(step == 0){
+               jump++ ;
+               if(i>=maxR){
+                   return -1 ;
+               }
+               
+               step = maxR-i;
+           }
+       }
+      
+      
+       }
+       
+       return 1;
+        
+    }
+}
